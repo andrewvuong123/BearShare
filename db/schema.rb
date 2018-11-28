b656aea737e3f97a4b428e4ff2528a63d638480c
yambda <edwardyam@berkeley.edu>
2018-11-28 15:05:21 -0800

    corrected migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_28_194329) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_194329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "isprivate"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_194329) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
-    t.text "text"
+    t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -68,12 +68,9 @@ ActiveRecord::Schema.define(version: 2018_11_28_194329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "group_user_roosters", "groups"
   add_foreign_key "group_user_roosters", "users"
-
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
-
   add_foreign_key "posts", "users"
 end
