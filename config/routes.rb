60574ef9160072a72eb380c232b6bd0cc00f38c0
ryanchankh <ryanchankh@berkeley.edu>
2018-11-28 20:29:17 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: "home#index"
+  get 'posts'
+  
   resources :group_user_roosters
   resources :groups
   resources :posts
-  get 'home/index'
+  
   devise_for :users
-  root to: "home#index"
+
   patch "/posts/:id/like", to: "posts#like", as: "like"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
